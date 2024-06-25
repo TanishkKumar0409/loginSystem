9f693222936d2c7843bebe565f30a230b2022845
TanishkKumar0409 <tanishkk60@gmail.com>
2024-06-25 21:18:31 +0530

    remove css and added tailwind css cdn for now

--- interfaces/registrationform.php
+++ interfaces/registrationform.php
@@ -5,40 +5,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<style>
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
 
-    body {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-    }
-
-    .login-form input {
-        display: block;
-        margin: 5%;
-    }
-</style>
-
-<body>
+<body class="flex content-center items-center h-screen">
     <section class="container">
-        <div class="">
-            <form action="script.php" class="login-form">
-                <label for="name">Name</label>
-                <input type="text" name="name" id="name">
-                <label for="phone">Phone</label>
-                <input type="number" name="phone" id="phone">
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email">
-                <label for="address">Address</label>
-                <input type="text" name="address" id="address">
-                <button name="submit-button">submit</button>
+        <div class="headings">
+            <h2>container</h2>
+        </div>
+        <div class="registration">
+            <form action="script.php" class="registration-form">
+                <label class="block" for="name">Name</label>
+                <input class="block border-2 border" type="text" name="name" id="name">
+                <label class="block" for="phone">Phone</label>
+                <input class="block border-2 border" type="number" name="phone" id="phone">
+                <label class="block" for="email">Email</label>
+                <input class="block border-2 border" type="email" name="email" id="email">
+                <label class="block" for="address">Address</label>
+                <input class="block border-2 border" type="text" name="address" id="address">
+                <button class="block border-2 border" name="submit-button">submit</button>
             </form>
         </div>
     </section>
