2996ef85ed08c536487e3a528c6aa01b4f7f4e94
TanishkKumar0409 <tanishkk60@gmail.com>
2024-06-26 21:03:18 +0530

    remove tailwindcss properly with all classes

--- interfaces/registrationform.php
+++ interfaces/registrationform.php
@@ -5,22 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
     <section class="container">
         <div class="registration">
             <form action="script.php" class="registration-form">
-                <label class="block" for="name">Name</label>
-                <input class="block border-2 border" type="text" name="name" id="name">
-                <label class="block" for="phone">Phone</label>
-                <input class="block border-2 border" type="number" name="phone" id="phone">
-                <label class="block" for="email">Email</label>
-                <input class="block border-2 border" type="email" name="email" id="email">
-                <label class="block" for="address">Address</label>
-                <input class="block border-2 border" type="text" name="address" id="address">
-                <button class="block border-2 border" name="submit-button">submit</button>
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name">
+                <label for="phone">Phone</label>
+                <input type="number" name="phone" id="phone">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email">
+                <label for="address">Address</label>
+                <input type="text" name="address" id="address">
+                <button name="submit-button">submit</button>
             </form>
         </div>
     </section>
