4961c68da1e34d3b9683ceb353d1c03807dfaf09
TanishkKumar0409 <tanishkk60@gmail.com>
2024-06-27 20:23:16 +0530

    make some changes in index.php

--- interfaces/index.php
+++ interfaces/index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <a href="registrationform.php">Registration</a>
-    <a href="loginform.php">Login</a>
+    <section class="container">
+        <div class="links">
+            <a href="registrationform.php">Registration</a>
+            <a href="loginform.php">Login</a>
+        </div>
+    </section>
 </body>
+
 </html>
