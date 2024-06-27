a33453e8cc59eb6a63bb3943c5695af719fb2ff0
TanishkKumar0409 <tanishkk60@gmail.com>
2024-06-27 20:18:52 +0530

    added dashboard page for show details

---
+++ interfaces/dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <section class="container">
+        <div class="dashborad-details">
+            <h2>Name: Tanishk Kumar</h2>
+            <h2>Phone: +91 95576 23131</h2>
+            <h2>Email: tanishkk60@gmail.com</h2>
+            <h2>Address: 198A, Shastrinagar, Seemadwar, Dehradun, Uttarakhand</h2>
+        </div>
+    </section>
+</body>
+
+</html>
