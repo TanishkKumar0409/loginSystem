277aaad17ab1a634bbcb20fdaaea273d6e04f6de
TanishkKumar0409 <tanishkk60@gmail.com>
2024-06-27 20:10:25 +0530

    make login form

---
+++ interfaces/loginform.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <section class="container">
+        <div class="loginform">
+            <form>
+                <label for="email">Email</label>
+                <input type="email" name="login-email" id="email">
+                <label for="password">Password</label>
+                <input type="password" name="login-password" id="password">
+                <button name="login-button">Login</button>
+            </form>
+        </div>
+    </section>
+</body>
+
+</html>
