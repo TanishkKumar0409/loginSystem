5a583f8f9f2fae938ed684eefeced49c5d2c38e2
TanishkKumar0409 <tanishkk60@gmail.com>
2024-07-23 21:22:46 +0530

    create some code for update

--- assets/php/session.php
+++ assets/php/session.php
@@ -1,15 +1,17 @@
 <?php
 include "config.php";
 session_start();
-$Semail=$_SESSION['email'];
-$Spassword=$_SESSION['password'];
-$select="SELECT * FROM users WHERE user_email='$Semail' AND user_password='$Spassword'";
-$selectquery=mysqli_query($connection,$select);
-$data=$selectquery->fetch_assoc();
-if($data>0){
-    $name=$data['user_name'];
-    $email=$data['user_email'];
-    $phone=$data['user_phone'];
-    $address=$data['user_address'];
-    $password=$data['user_password'];
+$Semail = $_SESSION['email']??$_SESSION['UPemail'];
+$Spassword = $_SESSION['password']??$_SESSION['UPpassword'];
+if ($Semail && $Spassword) {
+    $select = "SELECT * FROM users WHERE user_email='$Semail' AND user_password='$Spassword'";
+    $selectquery = mysqli_query($connection, $select);
+    $data = $selectquery->fetch_assoc();
+    if ($data > 0) {
+        $name = $data['user_name'];
+        $email = $data['user_email'];
+        $phone = $data['user_phone'];
+        $address = $data['user_address'];
+        $password = $data['user_password'];
+    }
 }
--- assets/php/updateScript.php
+++ assets/php/updateScript.php
@@ -1,17 +1,29 @@
 <?php
 include "config.php";
 include "session.php";
-if(isset($_POST['Cbtn'])){
-    $Cpassword=md5($_POST['Cpassword']);
-    if($Cpassword==$Spassword){
+if (isset($_POST['Cbtn'])) {
+    $Cpassword = md5($_POST['Cpassword']);
+    if ($Cpassword == $Spassword) {
         header("location:../../inner-pages/update.php");
     }
 }
-if(isset($_POST['Ubtn'])){
-    echo $Uname=$_POST['Uname'];
-    echo $Uemail=$_POST['Uemail'];
-    echo $Uphone=$_POST['Uphone'];
-    echo $Uaddress=$_POST['Uaddress'];
-    echo $Upassword=$_POST['Upassword'];
-    echo $ucpassword=$_POST['Ucpassword'];
+if (isset($_POST['Ubtn'])) {
+    $Uname = $_POST['Uname'];
+    $Uemail = $_POST['Uemail'];
+    $Uphone = $_POST['Uphone'];
+    $Uaddress = $_POST['Uaddress'];
+    $Upassword = md5($_POST['Upassword']);
+    $ucpassword = md5($_POST['Ucpassword']);
+    if ($Upassword == $ucpassword) {
+        $update = "UPDATE users SET user_name='$Uname', user_email='$Uemail' , user_phone='$Uphone' , user_address='$Uaddress' , user_password='$Upassword'  WHERE user_email='$Semail' AND user_password='$Spassword'";
+        $updatequery = mysqli_query($connection, $update);
+        session_start();
+        session_destroy();
+        session_start();
+        $_SESSION['UPemail']=$Uemail;
+        $_SESSION['UPpassword']=$Upassword;
+        header("location:../../inner-pages/dashboard");
+    } else {
+        echo "incorrect password ";
+    }
 }
